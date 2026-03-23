1fd15457cb661d9bde01f148584849c1af97a08f
Danielinh0 <danyeduardo0911@gmail.com>
2026-03-22 19:09:43 -0600

    Proyecto de cero
    Gracias a dios.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,26 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            SucursalSeeder::class,
-            SocioSeeder::class,
-            RutaSeeder::class,
-            ParadaSeeder::class,
-            UsuarioSeeder::class,
-            GerenteSeeder::class,
-            ChoferSeeder::class,
-            CajeroSeeder::class,
-            UrbanSeeder::class,
-            AsientoSeeder::class,
-            CorridaSeeder::class,
-            TramoSeeder::class,
-            TurnoSeeder::class,
-            TaquillaSeeder::class,
-            ClienteSeeder::class,
-            BoletoSeeder::class,
-            BoletoClienteSeeder::class,
-            BoletoPaqueteSeeder::class,
-            PaqueteSeeder::class,
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
