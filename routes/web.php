c48442b1a59958cbc11aa522cf14d442d89e683b
Gael Jafet Aquino Cruz <gaeljac7799@gmail.com>
2026-04-10 12:33:31 -0600

    Rutas weeeb
    Rutas para controladores agregados
    Asiento, Cliente, Usuario, venta

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    UrbanController,
-    CorridaController,
+    AsientoController,
     BoletoController,
-    RutaController
+    ClienteController,
+    CorridaController,
+    RutaController,
+    UrbanController,
+    UserController,
+    VentaController
 };
 
 
@@ -15,6 +19,82 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('asiento')->group(function () {
+        Route::controller(AsientoController::class)->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::get('/', 'store')->name('store');
+            Route::get('/{id}', 'show')->name('show');
+            Route::get('/{id}/edit', 'edit')->name('edit');
+            Route::get('/{id}', 'update')->name('update');
+            Route::get('/{id}', 'destroy')->name('destroy');
+        });
+    });
+
+    Route::prefix('boleto')->name('boleto.')->group(
+        function () {
+            Route::controller(BoletoController::class)->group(
+                function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/create', 'create')->name('create');
+                    Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
+                    Route::get('/{id}/edit', 'edit')->name('edit');
+                    Route::get('/{id}', 'update')->name('update');
+                    Route::get('/{id}', 'destroy')->name('destroy');
+                }
+            );
+        }
+    );
+
+    Route::prefix('cliente')->name('cliente.')->group(
+        function () {
+            Route::controller(ClienteController::class)->group(
+                function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/create', 'create')->name('create');
+                    Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
+                    Route::get('/{id}/edit', 'edit')->name('edit');
+                    Route::get('/{id}', 'update')->name('update');
+                    Route::get('/{id}', 'destroy')->name('destroy');
+                }
+            );
+        }
+    );
+
+    Route::prefix('corrida')->name('corrida.')->group(
+        function () {
+            Route::controller(CorridaController::class)->group(
+                function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/create', 'create')->name('create');
+                    Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
+                    Route::get('/{id}/edit', 'edit')->name('edit');
+                    Route::get('/{id}', 'update')->name('update');
+                    Route::get('/{id}', 'destroy')->name('destroy');
+                }
+            );
+        }
+    );
+
+    Route::prefix('ruta')->name('ruta.')->group(
+        function () {
+            Route::controller(RutaController::class)->group(
+                function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/create', 'create')->name('create');
+                    Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
+                    Route::get('/{id}/edit', 'edit')->name('edit');
+                    Route::get('/{id}', 'update')->name('update');
+                    Route::get('/{id}', 'destroy')->name('destroy');
+                }
+            );
+        }
+    );
 
     Route::prefix('urban')->name('urban.')->group(
         function () {
@@ -31,14 +111,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
             );
         }
     );
-    Route::prefix('corrida')->name('corrida.')->group(
+
+    Route::prefix('usuario')->name('usuario.')->group(
         function () {
-            Route::controller(CorridaController::class)->group(
+            Route::controller(UserController::class)->group(
                 function () {
                     Route::get('/', 'index')->name('index');
                     Route::get('/create', 'create')->name('create');
-                    Route::get('/{id}', 'show')->name('show');
                     Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
                     Route::get('/{id}/edit', 'edit')->name('edit');
                     Route::get('/{id}', 'update')->name('update');
                     Route::get('/{id}', 'destroy')->name('destroy');
@@ -46,30 +127,18 @@ Route::middleware(['auth', 'verified'])->group(function () {
             );
         }
     );
-    Route::prefix('boleto')->name('boleto.')->group(
+
+    Route::prefix('venta')->name('venta.')->group(
         function () {
-            Route::controller(BoletoController::class)->group(
+            Route::controller(VentaController::class)->group(
                 function () {
                     Route::get('/', 'index')->name('index');
                     Route::get('/create', 'create')->name('create');
-                    Route::get('/{id}', 'show')->name('show');
                     Route::get('/', 'store')->name('store');
+                    Route::get('/{id}', 'show')->name('show');
                     Route::get('/{id}/edit', 'edit')->name('edit');
                     Route::get('/{id}', 'update')->name('update');
                     Route::get('/{id}', 'destroy')->name('destroy');
-                }
-            );
-        }
-    );
-    Route::prefix('ruta')->name('ruta.')->group(
-        function () {
-            Route::controller(RutaController::class)->group(
-                function () {
-                    Route::get('/', 'index')->name('index');
-                    Route::get('/create', 'create')->name('create');
-                    Route::get('/{id}', 'show')->name('show');
-                    Route::get('/', 'store')->name('store');
-                    Route::get('/{id}/edit', 'edit')->name('edit');
                 }
             );
         }
