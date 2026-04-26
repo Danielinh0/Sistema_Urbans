080c24dd9e75e61ae4ba5c554e0014f010b1c620
Raul Chacon <raulchaconmelchor28@gmail.com>
2026-04-25 20:31:18 -0600

    Refactor user forms and tables for improved structure and accessibility

--- resources/views/components/usuario/⚡table.blade.php
+++ resources/views/components/usuario/⚡table.blade.php
@@ -68,27 +68,31 @@ new class extends Component
 
 <div>
     <livewire:barra-busqueda placeholder="Busca un Usuario..." />
-    <div>
-        <flux:card>
-            <flux:table :paginate="$this->usuarios">
-                <flux:table.columns>
-                    <flux:table.column sortable :sorted="$sortBy === 'id_usuario'" :direction="$sortDirection" wire:click="sort('id_usuario')">ID</flux:table.column>
-                    <flux:table.column sortable :sorted="$sortBy === 'name'" :direction="$sortDirection" wire:click="sort('name')">Nombre</flux:table.column>
-                    <flux:table.column sortable :sorted="$sortBy === 'email'" :direction="$sortDirection" wire:click="sort('email')">Email</flux:table.column>
-                    <flux:table.column sortable :sorted="$sortBy === 'id_sucursal'" :direction="$sortDirection" wire:click="sort('id_sucursal')">Sucursal</flux:table.column>
-                    <flux:table.column sortable :sorted="$sortBy === 'rol_nombre'" :direction="$sortDirection" wire:click="sort('rol_nombre')" >Tipo de usuario</flux:table.column>
-                    <flux:table.column >Direccion</flux:table.column>
-                    <flux:table.column>Acciones</flux:table.column>
-                </flux:table.columns>
-                <flux:table.rows>
-                    @forelse ($this->usuarios as $usuario)
-                        <flux:table.row :key="$usuario->id_usuario"> 
-                            <flux:table.cell>{{ $usuario->id_usuario }}</flux:table.cell>
-                            <flux:table.cell>{{ $usuario->name }} {{ $usuario->apellido_paterno }} {{ $usuario->apellido_materno }}</flux:table.cell>
-                            <flux:table.cell>{{ $usuario->email }}</flux:table.cell>
-                            <flux:table.cell>{{ $usuario->sucursal ? $usuario->sucursal->nombre : 'N/A' }}</flux:table.cell>
-                            <flux:table.cell>{{ $usuario->rol_nombre ?? 'N/A' }}</flux:table.cell>
-                            <flux:table.cell>
+    <flux:card>
+        <flux:table :paginate="$this->usuarios" horizontal>
+            <flux:table.columns>
+                <x-header-table sortable :sorted="$sortBy === 'id_usuario'" :direction="$sortDirection"
+                    wire:click="sort('id_usuario')">ID</x-header-table>
+                <x-header-table sortable :sorted="$sortBy === 'name'" :direction="$sortDirection"
+                    wire:click="sort('name')">Nombre</x-header-table>
+                <x-header-table sortable :sorted="$sortBy === 'email'" :direction="$sortDirection"
+                    wire:click="sort('email')">Email</x-header-table>
+                <x-header-table sortable :sorted="$sortBy === 'id_sucursal'" :direction="$sortDirection"
+                    wire:click="sort('id_sucursal')">Sucursal</x-header-table>
+                <x-header-table sortable :sorted="$sortBy === 'rol_nombre'" :direction="$sortDirection"
+                    wire:click="sort('rol_nombre')">Tipo de usuario</x-header-table>
+                <x-header-table icon="map-pin">Direccion</x-header-table>
+                <x-header-table align="center">Acciones</x-header-table>
+            </flux:table.columns>
+            <flux:table.rows>
+                @forelse ($this->usuarios as $usuario)
+                    <flux:table.row :key="$usuario->id_usuario"> 
+                        <flux:table.cell>{{ $usuario->id_usuario }}</flux:table.cell>
+                        <flux:table.cell>{{ $usuario->name }} {{ $usuario->apellido_paterno }} {{ $usuario->apellido_materno }}</flux:table.cell>
+                        <flux:table.cell>{{ $usuario->email }}</flux:table.cell>
+                        <flux:table.cell>{{ $usuario->sucursal ? $usuario->sucursal->nombre : 'N/A' }}</flux:table.cell>
+                        <flux:table.cell>{{ $usuario->rol_nombre ?? 'N/A' }}</flux:table.cell>
+                        <flux:table.cell>
                             {{$usuario->direccion->calle->nombre ?? ''}}
                             #{{$usuario->direccion->numero_exterior ?? ''}}, 
                             {{$usuario->direccion->calle->colonia->nombre ?? ''}}, 
@@ -96,32 +100,31 @@ new class extends Component
                             {{$usuario->direccion->calle->colonia->codigoPostal->estado->nombre ?? ''}},
                             {{$usuario->direccion->calle->colonia->codigoPostal->estado->pais->nombre ?? ''}}
                         </flux:table.cell>
-                            <flux:table.cell class="flex gap-2">
-                                <flux:button variant="ghost" icon="pencil" class="!text-azul_menu"
-                                    wire:click="$dispatch('preparar-edicion-usuario', { id: {{ $usuario->id_usuario }} })">
-                                    Editar
+                        <flux:table.cell class="flex gap-2">
+                            <flux:button variant="ghost" icon="pencil" class="!text-azul_menu"
+                                wire:click="$dispatch('preparar-edicion-usuario', { id: {{ $usuario->id_usuario }} })">
+                                <span class="hidden md:inline ml-1">Editar</span>
+                            </flux:button>
+                            @if (!$usuario->corridas->count()>0 || !$usuario->turnos->count()>0)
+                                <flux:button variant="ghost" icon="trash" class="!text-rojo_texto"
+                                    wire:click="$dispatch('preparar-eliminacion-usuario', { id: {{ $usuario->id_usuario }} })">
+                                    <span class="hidden md:inline ml-1">Eliminar</span>
                                 </flux:button>
-                                @if (!$usuario->corridas->count()>0 || !$usuario->turnos->count()>0)
-                                    <flux:button variant="ghost" icon="trash" class="!text-rojo_texto"
-                                        wire:click="$dispatch('preparar-eliminacion-usuario', { id: {{ $usuario->id_usuario }} })">
-                                        Eliminar
-                                    </flux:button>
-                                @endif
-                                
-                            </flux:table.cell>
+                            @endif
+                        </flux:table.cell>
 
-                        </flux:table.row>
-                    @empty
-                        <flux:table.row>
-                            <flux:table.cell colspan="7" class="text-center py-4 ">
-                                No se encontraron usuarios.
-                            </flux:table.cell>
-                        </flux:table.row>    
-                    @endforelse
+                    </flux:table.row>
+                @empty
+                    <flux:table.row>
+                        <flux:table.cell colspan="7" class="text-center py-4 ">
+                            No se encontraron usuarios.
+                        </flux:table.cell>
+                    </flux:table.row>    
+                @endforelse
 
-                </flux:table.rows>
-            </flux:table>
-        </flux:card>
-        <livewire:usuario.manager />
-    </div>
+            </flux:table.rows>
+        </flux:table>
+    </flux:card>
+    <livewire:usuario.manager />
+</div>
 </div>
